00ac90612a06ed21af1c5eeb9d71bbc8e3db4615
grant-madden <gm197018@ohio.edu>
2020-01-26 23:10:03 -0500

    Flashes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ include ArticlesHelper
     def create
         @article = Article.new(article_params)
         @article.save
+        flash.notice = "Article '#{@article.title}' Created!"
         redirect_to article_path(@article)
         
     end
@@ -25,12 +26,16 @@ include ArticlesHelper
 
     def destroy
         @article = Article.destroy(params[:id])
+        flash.notice = "Article '#{@article.title}' Deleted!"
         redirect_to action: 'index'
     end
 
     def update 
         @article = Article.find(params[:id])
         @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+
         redirect_to article_path(@article)
     end
 
